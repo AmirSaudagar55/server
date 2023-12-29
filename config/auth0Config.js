bccf60763532a68a5e1917ea5598ab64d4f15c7d
Amir Saudagar <121443390+AmirSaudagar55@users.noreply.github.com>
2023-12-29 18:57:30 +0530

    Update auth0Config.js

--- config/auth0Config.js
+++ config/auth0Config.js
@@ -1,7 +1,7 @@
 import {auth} from 'express-oauth2-jwt-bearer'
 
 const jwtCheck = auth({
-    audience: "http://localhost:8000",
+    audience: "https://server-beta-ruddy.vercel.app",
     issuerBaseURL: "https://dev-tye2gwc6gzgtkl5j.us.auth0.com",
     tokenSigningAlg: "RS256"
 })
